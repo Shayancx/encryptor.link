dd0ee7f6f12257121910d8920d42422d3666fdda
Shayancx <Shayan.Ashtarany@protonmail.com>
2025-06-07 21:09:42 +0200

    e

--- public/pgp-auth.js
+++
@@ -1,7 +0,0 @@
-async function signChallenge(privateKeyArmored, passphrase, nonce) {
-  const { readPrivateKey, decryptKey, sign } = openpgp;
-  const privateKey = await readPrivateKey({ armoredKey: privateKeyArmored });
-  const decrypted = await decryptKey({ privateKey, passphrase });
-  const signed = await sign({ message: await openpgp.createMessage({ text: nonce }), signingKeys: decrypted, detached: true });
-  return signed;
-}
