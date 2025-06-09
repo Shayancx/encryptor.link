5881911f76713e299143b4ef2c63d242c091f955
Shayancx <Shayan.Ashtarany@protonmail.com>
2025-06-09 14:01:43 +0200

    Typescript migration progress and optimization

--- app/javascript/services/error_service.js
+++
@@ -1,6 +0,0 @@
-export default class ErrorService {
-  static handle(error) {
-    console.error(error);
-    alert('Error: ' + error.message);
-  }
-}
--- app/javascript/services/validation_service.js
+++
@@ -1,7 +0,0 @@
-export default class ValidationService {
-  static validate({ message = '', ttl = 0, views = 0 }) {
-    if (ttl <= 0) return 'Invalid expiration time';
-    if (views <= 0) return 'Invalid view limit';
-    return null;
-  }
-}
