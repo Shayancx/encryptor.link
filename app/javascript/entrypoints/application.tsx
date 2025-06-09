bba5d55700757ed8b7936fd41fa3d57102f1e81e
Shayancx <Shayan.Ashtarany@protonmail.com>
2025-06-09 17:48:47 +0200

    Tailwind/ShadcnUI configuration

---
+++ app/javascript/entrypoints/application.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "@/components/App"
+import "@/entrypoints/application.css"
+
+import { Application } from "@hotwired/stimulus"
+import EncryptionController from "@/controllers/encryption_controller"
+import ThemeController from "@/controllers/theme_controller"
+import RichEditorController from "@/controllers/rich_editor_controller"
+import RateLimitController from "@/controllers/rate_limit_controller"
+import DataTableController from "@/controllers/data_table_controller"
+import "@/lib/csrf-helper"
+
+declare global {
+  interface Window {
+    Stimulus: Application
+  }
+}
+
+window.Stimulus = Application.start()
+window.Stimulus.register("encryption", EncryptionController)
+window.Stimulus.register("theme", ThemeController)
+window.Stimulus.register("rich-editor", RichEditorController)
+window.Stimulus.register("rate-limit", RateLimitController)
+window.Stimulus.register("data-table", DataTableController)
+
+const root = document.getElementById("root")
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
